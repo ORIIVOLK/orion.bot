f4e4583aaf0b082aa2b00a3b6992c774608dfa02
ORII VOLK <58107315+ORIIVOLK@users.noreply.github.com>
2022-01-08 21:40:07 +0000

    Update orionbot.js

--- orionbot.js
+++ orionbot.js
@@ -1,12 +1,40 @@
-const { Client, Intents } = require("discord.js")
-const client = new Client({intents: []});
+const prefix = "$"
+const { Client, Intents, StageChannel } = require("discord.js")
+
+const client = new Client({
+    intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+
+    ]
+})
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-});
-client.once('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
+
+    client.user.setActivity({name: "in Testing Stage", type: "PLAYING"})
+
+})
+
+client.on('messageCreate', (message) => {
+    
+    if(!message.content.startsWith(prefix))
+        return;
+
+    let arg = message.content.substring(prefix.length).split(" ");
+
+    switch (arg[0].toLowerCase()) {
+
+
+        case 'ping': {  
+             if (arg[1] === 'pong') {
+            message.channel.send("Hey! I was going to say that!")
+            } else {
+
+            message.channel.send("Pong!") }
+        }
+
     }
-});
-client.login(TOKEN);
+})
+
+client.login("token")
